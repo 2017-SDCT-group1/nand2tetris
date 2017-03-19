de532ec6a52679428188e980b31690e01ec4f1ea
cyj <chenyijie16@126.com>
2017-03-19 23:03:15 +0800

    assembler with no symbol finished

---
+++ chenyijie/06/Assembler 无符号版本/Assembler/main.cpp
@@ -0,0 +1,17 @@
+//本编译器处理无符号版本
+//不支持报错
+//不支持错误情况处理
+//默认输入汇编语言正确
+//忽略预定义符号
+
+#include <iostream>
+
+#include "assembler.h"
+using namespace std;
+
+int main()
+{
+	Assembler assembler;
+	assembler.Getline();
+	system("pause");
+}
