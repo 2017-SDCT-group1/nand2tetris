66c411c583425fc76bdeb45a6da7dc861b3ad252
WangYida16 <18801377618@163.com>
2017-03-30 17:18:56 +0800

    assembler without symbols

---
+++ WangYida/projects/06/Assembler0无符号/Assembler0/Assembler0/assembler0.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+void DealWithSingleRow();	//completed
+void Simlplify();			//completed
+void Translate();			//completed
+void DealWithA();			//completed
+void DealWithCJ();			//to do
+void DealWithCE();
+void Out();					//completed
+void Comp(char a[6]);
+
+int CommandType();			//completed
+
+#endif
